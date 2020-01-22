745d0f7ff4f784999875c23b9340206e1979cff8
mile-ku <mile.kuzmina@gmail.com>
2020-01-22 22:39:15 +0300

    Added Random Fortune Service

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = { 
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	};
+
+	private Random myRandom = new Random();
+	
+	@Override
+	public String getFortune() {
+		int i = myRandom.nextInt(data.length);
+		String theFortune = data[i];
+		return theFortune;
+	}
+
+}
--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,17 +1,15 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-	
+	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-	}
 	
 	@Override
 	public String getDailyWorkout() {
