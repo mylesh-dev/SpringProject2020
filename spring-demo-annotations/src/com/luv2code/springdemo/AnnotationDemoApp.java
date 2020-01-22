767f4400db805dc769acd17a29d2100923599ee4
mile-ku <mile.kuzmina@gmail.com>
2020-01-22 20:50:59 +0300

    Initial commit of project

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		context.close();
+	}
+
+}
