1920543f91f0049e058d33871e0200330b6d6abe
mile-ku <mile.kuzmina@gmail.com>
2020-02-11 16:20:03 +0300

    Configured Tomcat and Hibernate for MVC Project

---
+++ web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,28 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>List Customers</title>
+	
+</head>
+
+<body>
+
+	CRM - Customer Relationship Manager
+
+	
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
---
+++ web-customer-tracker/src/com/mylesh/springdemo/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.mylesh.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {	
+		return "list-customers";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
