3b7d9caa2bf73e6baba20c274d6dbc051ab75731
mile-ku <mile.kuzmina@gmail.com>
2020-02-12 18:29:24 +0300

    Added Delete button, full CRUD for Customer Tracker

--- web-customer-tracker/src/com/mylesh/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/mylesh/springdemo/controller/CustomerController.java
@@ -64,6 +64,13 @@ public class CustomerController {
 		// send over to our form		
 		return "customer-form";
 	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId")int theId) {
+		
+		customerService.deleteCustomer(theId);
+		return "redirect:/customer/list";
+	}
 		
 }
 
--- web-customer-tracker/src/com/mylesh/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/mylesh/springdemo/dao/CustomerDAOImpl.java
@@ -39,13 +39,22 @@ public class CustomerDAOImpl implements CustomerDAO {
 	@Override
 	public Customer getCustomer(int theId) {
 
-		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// now retrieve/read from database using the primary key
 		Customer theCustomer = currentSession.get(Customer.class, theId);
 		
 		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = 
+				currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();
 	}
 
 }
