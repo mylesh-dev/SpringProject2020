7b86469503f869379c0f0d235c9f8fb816547625
mile-ku <mile.kuzmina@gmail.com>
2020-02-21 21:02:00 +0300

    Get users from MySQL database, passwords encrypted

--- spring-security-demo-users/src/main/java/com/mylesh/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-users/src/main/java/com/mylesh/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
-package com.mylesh.springsecurity.demo.config;
+	package com.mylesh.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,16 +16,30 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
 	}
+	
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * 
+	 * UserBuilder users = User.withDefaultPasswordEncoder();
+	 * 
+	 * auth.inMemoryAuthentication()
+	 * .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+	 * .withUser(users.username("mary").password("test123").roles("EMPLOYEE",
+	 * "MANAGER"))
+	 * .withUser(users.username("susan").password("test123").roles("EMPLOYEE",
+	 * "ADMIN")); }
+	 */
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
